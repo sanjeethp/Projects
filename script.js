f65c1a1c96704bc2686f0373997f4262bc994c8a
Sanjeet-6266 <sanjeethp6266@gmail.com>
2024-12-28 01:34:16 +0530

    project-11
    as

--- script.js
+++ script.js
@@ -1,25 +1,8 @@
-var img = document.querySelector("img")
-var movex = 0
-var movey = 0
 
-document.addEventListener("keydown", (val) => {
-    if (val.code === "ArrowRight") {
-        movex++
-        img.style.left = movex + "%"
-    }
-    else if (val.code === "ArrowLeft") {
-        movex--
-        img.style.left = movex + "%"
-    }
-    else if (val.code === "ArrowUp") {
-        movey--
-        img.style.top = movey + "%"
-    }
-    else if (val.code === "ArrowDown") {
-        movey++
-        img.style.top = movey + "%"
-    }
-    else {
-        console.log("other")
-    }
+var btn = document.querySelectorAll("button")
+var body = document.querySelector("body")
+btn.forEach(function(val){
+    val.addEventListener("click",function(){
+       body.style.backgroundColor = val.innerHTML
+    })
 })
